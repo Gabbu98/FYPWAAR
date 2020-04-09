3152e80cc483326adef1bedd7a134bb93548e006
Gabbu98 <gabriel.camilleri.17@um.edu.mt>
2020-04-10 00:28:22 +0200

    truncated to SVD++

--- Dev/PythonDev/RecommenderSystem.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-
-import sklearn
-from sklearn.decomposition import TruncatedSVD
-
-columns = ['id','taskID','Rating']
-frame = pd.read_csv('DataVisitor.csv',sep='\t', names=columns)
-print(frame.head())
